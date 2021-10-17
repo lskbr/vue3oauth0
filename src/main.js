fef2d8c3dfae980fc3f63eb7dfeb06c3bcd88f1c
Nilo Menezes <nilo@logikraft.be>
2021-10-17 14:02:30 +0200

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+
+// Import the Auth0 configuration
+import authConfig from '../auth_config.json'
+
+// Import the plugin here
+import { setupAuth  } from "./auth";
+
+const app = createApp(App)
+
+app.use(router)
+
+function callbackRedirect(appState) {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : '/'
+    );
+  }
+  
+  setupAuth(authConfig, callbackRedirect).then((auth) => {
+    app.use(auth).mount('#app')
+    console.log("Done")
+  })
+
